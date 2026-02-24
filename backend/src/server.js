76c5fc2833e0d5bbc02a9f1c65dfc275dd91562a
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2026-02-24 14:48:44 +0000

    prepared for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 /*
-What's this file for ???
 setting up an express server  
 */
 import express from "express"          // go to package.json , add a type : "module" for this to work , ES module
@@ -7,7 +6,7 @@ import PaymentRoutes from "./routes/PaymentRoutes.js"
 import TenantRoutes from "./routes/TenantRoutes.js"
 import dotenv from "dotenv"
 import { connectDb } from "../config/db.js"
-
+import path from "path"
 import cors from "cors";
 
 
@@ -16,6 +15,10 @@ import cors from "cors";
 // this is how to initialize an express app 
 const app = express()
 
+// we need the frontend to run under the backend , so this is helpful 
+const __dirname = path.resolve()
+
+// to resolve the cors error 
 app.use(cors());
 
 dotenv.config(); // ; is required 
@@ -25,6 +28,15 @@ app.use(express.json());    // this will parse .json body : req/res
 
 app.use('/api/payments', PaymentRoutes)   // payments page : /api/payments 
 app.use('/api/tenants', TenantRoutes)   // tenants page : /api/tenants 
+
+// only in production 
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // app is listening on port 5001 
 connectDb().then(() => {
